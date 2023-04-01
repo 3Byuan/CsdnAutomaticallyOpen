86f930cd612be2e78180a2224ab8a14816abbf14
3Byuan <alakence@gmail.com>
2023-04-01 13:08:46 +0800

    Add selector for hexun.com

--- contentscript.js
+++ contentscript.js
@@ -1,4 +1,6 @@
 $(function () {
     $("#article_content").removeAttr("style");
     $(".hide-article-box").css("display", "none");
+    $('.art_contextBox').removeAttr('style');
+    $('.showAll').css('display', 'none');
 });
